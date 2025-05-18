e76e0e140c6a15996e7b978658abac6571c0d6d4
DavidJohannesS <davidjohannesSD@gmail.com>
2025-05-18 14:40:55 +0200

    structure update

--- backend/springboot/src/test/java/com/uni/TourplanterApplicationTests.java
+++ backend/springboot/src/test/java/com/uni/TourplanterApplicationTests.java
@@ -3,11 +3,11 @@ package com.uni;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TourplanterApplicationTests {
 
-	@Test
+/* 	@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
