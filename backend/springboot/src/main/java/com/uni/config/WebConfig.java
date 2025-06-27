ea8b5022316cd1c29bf14556907cdc2b10410bbc
DavidJohannesS <davidjohannesSD@gmail.com>
2025-06-27 15:38:38 +0200

    fix

--- backend/springboot/src/main/java/com/uni/config/WebConfig.java
+++ backend/springboot/src/main/java/com/uni/config/WebConfig.java
@@ -12,8 +12,8 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**") // Dein API-Pfad
-                        .allowedOrigins("http://localhost:5173") // React-Dev-Server (Vite)
+                registry.addMapping("/**") // Dein API-Pfad
+                        .allowedOrigins("*") // React-Dev-Server (Vite)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
